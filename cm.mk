d76596f12da7e21dbbab015e51218f386dc0df5a
JohnHoder <hoder.john@gmail.com>
2015-11-22 01:04:45 +0100

    Initial commit (Cloned)

---
+++ cm.mk
@@ -0,0 +1,27 @@
+# Include GSM stuff
+$(call inherit-product, vendor/cm/config/gsm.mk)
+
+# Inherit device configuration for p506.
+$(call inherit-product, device/lge/p506/p506.mk)
+
+TARGET_SCREEN_HEIGHT := 480
+TARGET_SCREEN_WIDTH := 320
+TARGET_BOOTANIMATION_HALF_RES := true
+$(call inherit-product, vendor/cm/config/mini.mk)
+
+# Overrides
+PRODUCT_NAME := cm_p506
+PRODUCT_BRAND := lge
+PRODUCT_DEVICE := p506
+PRODUCT_MODEL := LG-P506
+PRODUCT_MANUFACTURER := LGE
+PRODUCT_CHARACTERISTICS := phone
+
+# Release name and versioning
+PRODUCT_RELEASE_NAME := OptimusThrive
+PRODUCT_VERSION_DEVICE_SPECIFIC :=
+
+PRODUCT_BUILD_PROP_OVERRIDES += \
+    PRODUCT_NAME=lge_p506 \
+    BUILD_FINGERPRINT="lge/thunderg/thunderg:2.3.3/GRJ22/LG-P506-V11f.19E55B0D28:user/release-keys" \
+    PRIVATE_BUILD_DESC="thunderg-user 2.3.3 GRJ22 LG-P506-V11f.19E55B0D28 release-keys"
---
+++ p506.mk
@@ -0,0 +1,74 @@
+# p506 configs
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/configs/thunder_keypad.kl:system/usr/keylayout/thunder_keypad.kl \
+    $(LOCAL_PATH)/configs/thunder_keypad.kcm.bin:system/usr/keychars/thunder_keypad.kcm.bin \
+    $(LOCAL_PATH)/configs/touch_mcs6000.idc:system/usr/idc/touch_mcs6000.idc \
+    $(LOCAL_PATH)/configs/wpa_supplicant.conf:system/etc/wifi/wpa_supplicant.conf
+
+# p506 init
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/init.p506.rc:root/init.p506.rc \
+    $(LOCAL_PATH)/ueventd.p506.rc:root/ueventd.p506.rc \
+    $(LOCAL_PATH)/fstab.p506:root/fstab.p506
+
+# fm radio
+#PRODUCT_PACKAGES += \
+#    Effem \
+#    libfmradio.bcm4325
+#
+#PRODUCT_COPY_FILES += \
+#    frameworks/base/data/etc/com.stericsson.hardware.fm.receiver.xml:system/etc/permissions/com.stericsson.hardware.fm.receiver.xml
+
+# Don't include charger detection in recovery mode
+ifneq (eng,$(TARGET_BUILD_VARIANT))
+PRODUCT_COPY_FILES += $(LOCAL_PATH)/checkbootreason:root/sbin/checkbootreason
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/chargemode/chargerimages/battery_ani_01.rle:root/chargerimages/battery_ani_01.rle \
+    $(LOCAL_PATH)/chargemode/chargerimages/battery_ani_02.rle:root/chargerimages/battery_ani_02.rle \
+    $(LOCAL_PATH)/chargemode/chargerimages/battery_ani_03.rle:root/chargerimages/battery_ani_03.rle \
+    $(LOCAL_PATH)/chargemode/chargerimages/battery_ani_04.rle:root/chargerimages/battery_ani_04.rle \
+    $(LOCAL_PATH)/chargemode/chargerimages/battery_ani_05.rle:root/chargerimages/battery_ani_05.rle \
+    $(LOCAL_PATH)/chargemode/chargerimages/battery_bg.rle:root/chargerimages/battery_bg.rle \
+    $(LOCAL_PATH)/chargemode/chargerimages/battery_charging_01.rle:root/chargerimages/battery_charging_01.rle \
+    $(LOCAL_PATH)/chargemode/chargerimages/battery_charging_02.rle:root/chargerimages/battery_charging_02.rle \
+    $(LOCAL_PATH)/chargemode/chargerimages/battery_charging_03.rle:root/chargerimages/battery_charging_03.rle \
+    $(LOCAL_PATH)/chargemode/chargerimages/battery_charging_04.rle:root/chargerimages/battery_charging_04.rle \
+    $(LOCAL_PATH)/chargemode/chargerimages/battery_charging_05.rle:root/chargerimages/battery_charging_05.rle \
+    $(LOCAL_PATH)/chargemode/chargerimages/battery_charging_06.rle:root/chargerimages/battery_charging_06.rle \
+    $(LOCAL_PATH)/chargemode/chargerimages/battery_wait_ani_01.rle:root/chargerimages/battery_wait_ani_01.rle \
+    $(LOCAL_PATH)/chargemode/chargerimages/battery_wait_ani_02.rle:root/chargerimages/battery_wait_ani_02.rle \
+    $(LOCAL_PATH)/chargemode/chargerimages/black_bg.rle:root/chargerimages/black_bg.rle \
+    $(LOCAL_PATH)/chargemode/chargerlogo:root/sbin/chargerlogo
+endif
+
+# P506 bluetooth vendor configuration
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/bluetooth/bt_vendor.conf:system/etc/bluetooth/bt_vendor.conf
+
+#P500_SPEAKER_IN_CALL_FIX
+PRODUCT_COPY_FILES += $(LOCAL_PATH)/configs/audio_policy.conf:system/etc/audio_policy.conf
+
+# Live wallpapers
+PRODUCT_COPY_FILES += packages/wallpapers/LivePicker/android.software.live_wallpaper.xml:/system/etc/permissions/android.software.live_wallpaper.xml
+PRODUCT_PACKAGES += \
+    LiveWallpapers \
+    LiveWallpapersPicker \
+    VisualizationWallpapers \
+    librs_jni
+
+# Inherit products (Most specific first)
+$(call inherit-product, vendor/lge/p506/p506-vendor.mk)
+$(call inherit-product, device/lge/msm7x27-common/device.mk)
+$(call inherit-product, vendor/lge/msm7x27-common/msm7x27-common-vendor-blobs.mk)
+
+# Overrides
+PRODUCT_NAME := p506
+PRODUCT_DEVICE := p506
+PRODUCT_MODEL := LG-P506
+PRODUCT_MANUFACTURER := LGE
+
+PRODUCT_AAPT_PREF_CONFIG := mdpi
+$(call inherit-product, device/mdpi-common/mdpi.mk)
+
+# P506 overlays (Most specific last)
+DEVICE_PACKAGE_OVERLAYS += device/lge/p506/overlay
